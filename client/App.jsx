ad7829ec1b531024a2e00162bc2eaafe3005e0bf
Ariel Maor <arielmaor@Ariels-MacBook-Pro.local>
2023-12-27 13:13:21 -0800

    231227_ReactComponents_1312_Ari

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Forum from './components/Forum';
+// import FishInfo from './components/FishInfo';
+
 //need to import the BrowserRouter, Route, Link from react-router-dom
 //need to import Navbar file if we have it in a separate file
 //need to import Home page file if in separate file
@@ -19,26 +27,24 @@ import './App.css'
 
 function App() {
   return (
-    <h1>hello!</h1>
-    // <BrowserRouter>
-    
-    //   <div>
-    //     <Navbar />
-    //     <Route path="/" exact component={Home} />
-    //     <Route path="/login" component={Login} />
-    //     <Route path="/register" component={Register} />
-    //     <Route path="/feed" component={Feed} />
-    //     <Route path="/messaging" component={Messaging} />
-    //     <Route path="/chat"  component={Chat} />
-    //     <div>
-    //       <Link to="/login"><button>Login</button></Link>
-    //       <Link to="/register"><button>Login</button></Link>
-    //       <Link to="/feed"><button>Login</button></Link>
-    //       <Link to="/messaging"><button>Login</button></Link>
-    //       <Link to="/chat"><button>Login</button></Link>
-    //     </div>
-    //   </div>
-    // </BrowserRouter>      
+    <><h1>hello!</h1><BrowserRouter>
+
+      <div>
+        <Navbar />
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/Forum" component={Forum} />
+        {/* <Route path="/FishInfo" component={} /> */}
+        {/* <div>
+          <Link to="/login"><button>Login</button></Link>
+          <Link to="/register"><button>Login</button></Link>
+          <Link to="/feed"><button>Login</button></Link>
+          <Link to="/messaging"><button>Login</button></Link>
+          <Link to="/chat"><button>Login</button></Link>
+        </div> */}
+      </div>
+    </BrowserRouter></>      
 
   );
 };
--- client/main.jsx
+++ client/main.jsx
@@ -1,23 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
 
-
-
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './client/App.jsx';
-// import './css/styles.css';
-// //import * as bootstrap from 'bootstrap'
-
-// const container = document.getElementById('root');
-// const root = createRoot(container);
-
-// root.render(<App tab="/"/>
+root.render(<App />);
